d909b9ada9ee9354210c7d1858bdfa7899cff672
Fellipe Araujo <araujo.fellipe@hotmail.com>
2022-03-09 14:43:47 -0300

    Fix identation

--- App.tsx
+++ App.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { Home } from './src/pages/Home';
 
-export default function App(){
+export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <Home/>
+      <Home />
     </>
-  )
+  );
 }
